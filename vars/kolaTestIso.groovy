2eb48473a34b4d5d6cf269b8757582166e336f21
Dusty Mabe <dusty@dustymabe.com>
2022-10-12 23:32:29 -0400

    {kola,kolaTestIso}: initial stab at multi-arch support
    The rule here is mostly to run things through either `cosa` commands
    or run generic bash commands through `cosa shell --`. This also is
    made easier if we're running the `cosa` command within `shwrap()`
    from the cosa dir already, so let's use `dir(cosaDir)` to try to
    enable that simplification.

--- vars/kolaTestIso.groovy
+++ vars/kolaTestIso.groovy
@@ -1,5 +1,6 @@
 // Run kola testiso
 // Available parameters:
+//     arch:               string  -- the target architecture
 //     cosaDir:            string  -- cosa working directory
 //     extraArgs:          string  -- extra arguments to pass to `kola testiso`
 //     extraArgs4k:        string  -- extra arguments to pass to 4k `kola testiso`
@@ -14,6 +15,7 @@
 //     skipUEFI:           boolean -- skip UEFI tests
 //     marker:             string  -- some identifying text to add to uploaded artifact filenames
 def call(params = [:]) {
+    def arch = params.get('arch', "x86_64");
     def cosaDir = utils.getCosaDir(params)
     def extraArgs = params.get('extraArgs', "");
     def extraArgs4k = params.get('extraArgs4k', "");
@@ -32,7 +34,7 @@ def call(params = [:]) {
     def token = shwrapCapture("uuidgen | cut -f1 -d-")
 
     // Create a unique output directory for this run of fcosKola
-    def outputDir = shwrapCapture("mktemp -d ${cosaDir}/tmp/kolaTestIso-XXXXX")
+    def outputDir = shwrapCapture("cosa shell -- mktemp -d ${cosaDir}/tmp/kolaTestIso-XXXXX")
 
     // list of identifiers for each run for log collection
     def Ids = []
@@ -43,42 +45,52 @@ def call(params = [:]) {
         def id = marker == "" ? "kola-testiso-metal" : "kola-testiso-metal-${marker}"
         Ids += id
         def scenariosArg = scenarios == "" ? "" : "--scenarios ${scenarios}"
-        shwrap("cd ${cosaDir} && kola testiso -S ${extraArgs} ${scenariosArg} --output-dir ${outputDir}/${id}")
+        shwrap("cosa kola testiso -S ${extraArgs} ${scenariosArg} --output-dir ${outputDir}/${id}")
     }
     if (!params['skipMetal4k']) {
-        testIsoRuns1["metal4k"] = {
-            def id = marker == "" ? "kola-testiso-metal4k" : "kola-testiso-metal4k-${marker}"
-            Ids += id
-            def scenariosArg = scenarios4k == "" ? "" : "--scenarios ${scenarios}"
-            scenariosArg = scenarios4k == "" ? "" : "--scenarios ${scenarios}"
-            shwrap("cd ${cosaDir} && kola testiso -S --qemu-native-4k ${extraArgs4k} ${scenariosArg} --output-dir ${outputDir}/${id}")
+        // metal4k test doesn't work on s390x for now
+        // https://github.com/coreos/fedora-coreos-tracker/issues/1261
+        // and testiso for s390x doesn't support iso installs either
+        if (arch != 's390x') {
+            testIsoRuns1["metal4k"] = {
+                def id = marker == "" ? "kola-testiso-metal4k" : "kola-testiso-metal4k-${marker}"
+                Ids += id
+                def scenariosArg = scenarios4k == "" ? "" : "--scenarios ${scenarios}"
+                scenariosArg = scenarios4k == "" ? "" : "--scenarios ${scenarios}"
+                shwrap("cosa kola testiso -S --qemu-native-4k ${extraArgs4k} ${scenariosArg} --output-dir ${outputDir}/${id}")
+            }
         }
     }
     if (!params['skipMultipath']) {
         testIsoRuns2["multipath"] = {
             def id = marker == "" ? "kola-testiso-multipath" : "kola-testiso-multipath-${marker}"
             Ids += id
-            shwrap("cd ${cosaDir} && kola testiso -S --qemu-multipath ${extraArgsMultipath} --scenarios ${scenariosMultipath} --output-dir ${outputDir}/${id}")
+            shwrap("cosa kola testiso -S --qemu-multipath ${extraArgsMultipath} --scenarios ${scenariosMultipath} --output-dir ${outputDir}/${id}")
         }
     }
     if (!params['skipUEFI']) {
-        testIsoRuns2["metalUEFI"] = {
-            def id = marker == "" ? "kola-testiso-uefi" : "kola-testiso-uefi-${marker}"
-            Ids += id
-            shwrap("mkdir -p tmp/${id}")
-            shwrap("cd ${cosaDir} && kola testiso -S --qemu-firmware=uefi ${extraArgsUEFI} --scenarios ${scenariosUEFI} --output-dir tmp/${id}/insecure")
-            shwrap("cd ${cosaDir} && kola testiso -S --qemu-firmware=uefi-secure ${extraArgsUEFI} --scenarios ${scenariosUEFI} --output-dir tmp/${id}/secure")
+        // only aarch64 and x86_64 support UEFI
+        if (arch in ['aarch64', 'x86_64']) {
+            testIsoRuns2["metalUEFI"] = {
+                def id = marker == "" ? "kola-testiso-uefi" : "kola-testiso-uefi-${marker}"
+                Ids += id
+                shwrap("cosa shell -- mkdir -p tmp/${id}")
+                shwrap("cosa kola testiso -S --qemu-firmware=uefi ${extraArgsUEFI} --scenarios ${scenariosUEFI} --output-dir tmp/${id}/insecure")
+                shwrap("cosa kola testiso -S --qemu-firmware=uefi-secure ${extraArgsUEFI} --scenarios ${scenariosUEFI} --output-dir tmp/${id}/secure")
+            }
         }
     }
 
     stage("Test Live Images") {
-        try {
-            parallel(testIsoRuns1)
-            parallel(testIsoRuns2)
-        } finally {
-            for (id in Ids) {
-                shwrap("tar -c -C ${outputDir} ${id} | xz -c9 > ${env.WORKSPACE}/${id}-${token}.tar.xz")
-                archiveArtifacts allowEmptyArchive: true, artifacts: "${id}-${token}.tar.xz"
+        dir(cosaDir) {
+            try {
+                parallel(testIsoRuns1)
+                parallel(testIsoRuns2)
+            } finally {
+                for (id in Ids) {
+                    shwrap("cosa shell -- tar -c --xz ${outputDir}/${id} > ${env.WORKSPACE}/${id}-${token}.tar.xz")
+                    archiveArtifacts allowEmptyArchive: true, artifacts: "${id}-${token}.tar.xz"
+                }
             }
         }
     }
