a97a79791aaa56a352f2ed47b6cbdf6276b46a6f
Dusty Mabe <dusty@dustymabe.com>
2023-08-23 12:16:24 -0400

    test

--- vars/kola.groovy
+++ vars/kola.groovy
@@ -102,42 +102,6 @@ def call(params = [:]) {
             args += " --exttest=${env.WORKSPACE}/${path}"
         }
 
-        if (platformArgs != "" || extraArgs != "") {
-            // There are two cases where we land here:
-            //   1. The user passed `platformArgs`, which implies we're
-            //      running cloud platform (non qemu) tests and don't need to
-            //      worry about basic-qemu-scenarios or resource usage so we
-            //      don't need to run reprovision tests separately.
-            //   2. The user passed `extraArgs`. In that case they want more
-            //      control over the kola run that might conflict with
-            //      the --tag arguments we provide below. Let's just
-            //      do a single run in that case.
-            id = marker == "" ? "kola" : "kola-${marker}"
-            ids += id
-            runKola(id, 'run', "--parallel=${parallel} ${args} ${extraArgs}")
-        } else {
-            // basic run
-            if (!params['skipBasicScenarios']) {
-                id = marker == "" ? "kola-basic" : "kola-basic-${marker}"
-                ids += id
-                def skipSecureBootArg = ""
-                if (params['skipSecureBoot']) {
-                    skipSecureBootArg = "--skip-secure-boot"
-                }
-                runKola(id, 'run', "--basic-qemu-scenarios ${skipSecureBootArg}")
-            }
-            // normal run (without reprovision tests because those require a lot of memory)
-            id = marker == "" ? "kola" : "kola-${marker}"
-            ids += id
-            runKola(id, 'run', "--tag='!reprovision' --parallel=${parallel} ${args}")
-
-            // re-provision tests (not run with --parallel argument to kola)
-            id = marker == "" ? "kola-reprovision" : "kola-reprovision-${marker}"
-            ids += id
-            runKola(id, 'run', "--tag='reprovision' ${args}")
-        }
-    }
-
     if (!params["skipUpgrade"]) {
         kolaRuns["${titleMarker}kola:upgrade"] = {
             // If upgrades are broken `cosa kola --upgrades` might
